0e6e423ec5d6016b5a0353cd31472578d7c529ce
mikeforrestharder <mikeforrestharder@gmail.com>
2016-01-08 12:41:32 -0800

    initial setup

---
+++ dist/app.js
@@ -0,0 +1,21 @@
+System.register([], function (_export) {
+  "use strict";
+
+  var App;
+
+  function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+  return {
+    setters: [],
+    execute: function () {
+      App = function App() {
+        _classCallCheck(this, App);
+
+        this.message = "Coming Soon";
+      };
+
+      _export("App", App);
+    }
+  };
+});
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImFwcC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7TUFBYSxHQUFHOzs7Ozs7O0FBQUgsU0FBRyxHQUNILFNBREEsR0FBRyxHQUNBOzhCQURILEdBQUc7O0FBRVosWUFBSSxDQUFDLE9BQU8sR0FBRyxhQUFhLENBQUM7T0FDOUIiLCJmaWxlIjoiYXBwLmpzIiwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGNsYXNzIEFwcCB7XG4gIGNvbnN0cnVjdG9yKCkge1xuICAgIHRoaXMubWVzc2FnZSA9IFwiQ29taW5nIFNvb25cIjtcbiAgfVxufSJdLCJzb3VyY2VSb290IjoiL3NvdXJjZS8ifQ==
